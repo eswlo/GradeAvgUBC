fe429d03e19a694ff9269185fc99c211aebc7545
eswlo <sl3923@columbia.edu>
2025-01-01 22:59:42 -0800

    Add and style pagination

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -5,12 +5,14 @@ import "slick-carousel/slick/slick-theme.css";
 import styles from "./Carousel.module.css";
 
 import { AvgObj } from "../../api";
+import { useEffect, useState } from "react";
 
 interface CarouselProps {
   avgListGroup: AvgObj[][];
 }
 
 const Carousel: React.FC<CarouselProps> = (props) => {
+  const [currentSlideNumber, setCurrentSlideNumber] = useState<number>(1);
   const totalSlideCount = props.avgListGroup.length;
   const hideDotsShowPagination: boolean = totalSlideCount > 30;
   const settings = {
@@ -21,18 +23,36 @@ const Carousel: React.FC<CarouselProps> = (props) => {
     slidesToScroll: 1,
   };
 
+  const pagination = () => {
+    // console.log(`currentSlideNumber: ${currentSlideNumber}`);
+    if (props.avgListGroup.length > 0 && hideDotsShowPagination) {
+      return (
+        <div className={styles.pagination}>
+          <p>{`slide ${currentSlideNumber} of ${totalSlideCount}`}</p>
+        </div>
+      )
+    } else {
+      return (
+        <div className={styles.pagination}>
+        </div>
+      )
+    }
+  };
+
+
   
   return (
     <div className={styles.sliderContainer}>
-      <Slider {...settings}>
+      <Slider {...settings} beforeChange={(_: number, nextSlide: number) => setCurrentSlideNumber(nextSlide+1)}>
         {props.avgListGroup.map((list, index) => (
           <div key={index} className={styles.slide}>
             <Card fetchedAvgGrades={list}/>
           </div>
         ))}
       </Slider>
+      {pagination()} 
     </div>
-    
+
   );
 };
 
